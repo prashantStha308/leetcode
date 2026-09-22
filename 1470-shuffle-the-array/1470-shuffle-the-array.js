6fa34769bc914c197ab96610a5db671d1390e8a9
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2026-09-22 21:25:12 +0545

    Time: 58 ms (26.5%), Space: 58 MB (41%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -4,17 +4,13 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    // use two pointers for this
-    // one that looks from begenning of the arr, another from nth place
-
-    // let left = 0, right = n;
+    let left = 0, right = n;
     let arr = [];
-    let count = 0;
+    let i = 0;
 
-    for(let i = 0; i < n; i++){
-        arr[count + i] = nums[i];
-        arr[count + i+1] = nums[n + i];
-        count++;
+    while(left < n){
+        arr[i++] = nums[left++];
+        arr[i++] = nums[right++];
     }
 
     return arr;
